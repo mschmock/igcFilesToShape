37c1da6395d4ecb6e8e49c51f0b0f01f7aa2c478
Manuel Schmocker <manuel.schmocker@gmail.com>
2022-09-02 20:04:34 +0200

    v0.9 first step 02.09.2022
    a long way to go

--- src/main/java/ch/manuel/igctoshape/Startup.java
+++ src/main/java/ch/manuel/igctoshape/Startup.java
@@ -4,10 +4,19 @@
  */
 package ch.manuel.igctoshape;
 
+import ch.manuel.utilities.MyUtilities;
+
 /**
  *
  * @author manue
  */
 public class Startup {
+  
+      public static void main(String[] args) {
+        
+        // Set Look and Feel
+        MyUtilities.setLaF("Windows");
+        
+      }
   
 }
