5f58cf29a3c5d909737e2d5f167d43c7b2cc2991
Manuel Schmocker <manuel.schmocker@gmail.com>
2022-09-24 12:41:27 +0200

    v0.91 igc data handling 23.09.2022
    gui working
    set up igc data handling

--- src/main/java/ch/manuel/igctoraster/IGCprocessing.java
+++ src/main/java/ch/manuel/igctoraster/IGCprocessing.java
@@ -2,7 +2,6 @@
 // Datum: 22.09.2022
 package ch.manuel.igctoraster;
 
-import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.io.BufferedReader;
 import java.io.File;
@@ -20,7 +19,7 @@ public class IGCprocessing {
 
   // MEMBERVARIABLES
   private BufferedReader reader;
-  private List<Point2D.Float> listLonLat;    
+  private List<Point2D.Double> listLonLat;
 
   // Regex
   private static final Pattern B_RECORD_RE = Pattern.compile("^B(\\d{6})(\\d{2})(\\d{5})([NS])(\\d{3})(\\d{5})([EW])([AV])(\\d{5})(\\d{5})");
@@ -30,7 +29,6 @@ public class IGCprocessing {
       group 5+6: longitude
       group 9+10: altitude
    */
-
   public IGCprocessing(File file) {
     listLonLat = new ArrayList<>();
 
@@ -41,6 +39,11 @@ public class IGCprocessing {
     }
   }
 
+  // return list
+  public List<Point2D.Double> getPointList() {
+    return listLonLat;
+  }
+  
   public void processIGC() {
 
     StringBuilder content = new StringBuilder();
@@ -55,28 +58,56 @@ public class IGCprocessing {
         if (ma.find()) {
 
           // S + N         
-          float y = Integer.parseInt(ma.group(2), 10) + Integer.parseInt(ma.group(3), 10) / 60000f;
+          double y = Integer.parseInt(ma.group(2), 10) + Integer.parseInt(ma.group(3), 10) / 60000.0;
           if (ma.group(4).compareTo("S") == 0) {
             y = -y;
           }
           // E + W
-          float x = Integer.parseInt(ma.group(5), 10) + Integer.parseInt(ma.group(6), 10) / 60000f;
+          double x = Integer.parseInt(ma.group(5), 10) + Integer.parseInt(ma.group(6), 10) / 60000.0;
           if (ma.group(7).compareTo("W") == 0) {
             x = -x;
           }
           // store in list 
-          listLonLat.add(new Point2D.Float(x, y));
+          // in swiss projection LV95
+          Point2D.Double pt = wgs84ToLV95(new Point2D.Double(x, y) );
+          listLonLat.add( new Point2D.Double(x, y) );
         }
 
 //      content.append(line);
 //      content.append(System.lineSeparator());
       }
 
-      
 //    return content.toString();
     } catch (IOException ex) {
       Logger.getLogger(IGCprocessing.class.getName()).log(Level.SEVERE, null, ex);
     }
   }
 
+  // Transformation of ellipsoidal WGS84 coordinates to Swiss projection coordinates
+  // Source: https://www.swisstopo.admin.ch/
+  public static Point2D.Double wgs84ToLV95(Point2D.Double pt) {
+    // latitudes φ and longitudes λ into arcseconds
+    double φ = pt.getY() * 3600;
+    double λ = pt.getX() * 3600;
+
+    // calculate the auxiliary values 
+    φ = (φ - 169028.66) / 10000;
+    λ = (λ - 26782.5) / 10000;
+
+    // Calculate projection coordinates in LV95
+    double e_lv95 = 2600072.37
+            + +211455.93 * λ
+            - 10938.51 * λ * φ
+            - 0.36 * λ * Math.pow(φ, 2)
+            - 44.54 * Math.pow(λ, 3);
+
+    double n_lv95 = 1200147.07
+            + 308807.95 * φ
+            + 3745.25 * Math.pow(λ, 2)
+            + 76.63 * Math.pow(φ, 2)
+            - 194.56 * Math.pow(λ, 2) * φ
+            + 119.79 * Math.pow(φ, 3);
+
+    return new Point2D.Double(e_lv95, n_lv95);
+  }
 }
